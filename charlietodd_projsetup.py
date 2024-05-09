41fe3d2de0da18122901d9b4e0c9cbe883b65c79
Charlie <168452029+charlietodd11@users.noreply.github.com>
2024-05-08 23:10:05 -0500

    Create charlietodd_projsetup.py

---
+++ charlietodd_projsetup.py
@@ -0,0 +1,6 @@
+'''This module provides functions for creating a series of project folders'''
+#Import dependencies
+import math
+import statistics
+import charlietodd_utils.py
+def create_folders_for_range(start, end):
